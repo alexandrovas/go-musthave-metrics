608e71d93a94d3d3464ac3576e9a7a600e5ae313
Alexander Alexandrov <alex.mirenki@gmail.com>
2026-07-10 15:53:12 +0300

    fix: move mutex to struct field
    Signed-off-by: Alexander Alexandrov <alex.mirenki@gmail.com>

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -9,7 +9,7 @@ import (
 type MemStorage struct {
 	gauges   map[string]float64
 	counters map[string]int64
-	sync.RWMutex
+	mu       sync.Mutex
 }
 
 func NewMemStorage() *MemStorage {
@@ -20,35 +20,40 @@ func NewMemStorage() *MemStorage {
 }
 
 func (s *MemStorage) SetGauge(name string, value float64) {
-	s.Lock()
-	defer s.Unlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	s.gauges[name] = value
 }
 
 func (s *MemStorage) GetGauge(name string) (float64, bool) {
-	s.RLock()
-	defer s.RUnlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	v, ok := s.gauges[name]
 	return v, ok
 }
 
 func (s *MemStorage) AddCounter(name string, delta int64) {
-	s.Lock()
-	defer s.Unlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	s.counters[name] += delta
 }
 
 func (s *MemStorage) GetCounter(name string) (int64, bool) {
-	s.RLock()
-	defer s.RUnlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	v, ok := s.counters[name]
 	return v, ok
 }
 
 // Gauges возвращает копию всех gauge-метрик.
 func (s *MemStorage) Gauges() map[string]float64 {
-	s.RLock()
-	defer s.RUnlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	cp := make(map[string]float64, len(s.gauges))
 	maps.Copy(cp, s.gauges)
 	return cp
@@ -56,8 +61,9 @@ func (s *MemStorage) Gauges() map[string]float64 {
 
 // Counters возвращает копию всех counter-метрик.
 func (s *MemStorage) Counters() map[string]int64 {
-	s.RLock()
-	defer s.RUnlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	cp := make(map[string]int64, len(s.counters))
 	maps.Copy(cp, s.counters)
 	return cp
@@ -65,7 +71,8 @@ func (s *MemStorage) Counters() map[string]int64 {
 
 // Служебная функция для вывода в консоль текущего состояния хранилища
 func (s *MemStorage) Log() {
-	s.RLock()
-	defer s.RUnlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	slog.Debug("storage state", "gauges", s.gauges, "counters", s.counters)
 }
